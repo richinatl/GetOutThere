49d014b521cf0cf4fbcfd5af85a6cf2d775ba509
Richard Murphy <richinatl@gmail.com>
2022-12-26 11:40:47 -0500

    added layout and nav functions, planned context use

---
+++ get-out-there/src/components/layout/MainNavigation.js
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
+
+function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.title}>Get Out There</div>
+      <nav className={classes.nav__container}>
+        <ul>
+          <li className={classes.nav__li}>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li className={classes.nav__li}>
+            <Link to="/new-event">Add an event</Link>
+          </li>
+          <li className={classes.nav__li}>
+            <Link to="/favorites">
+              These sound good
+              <span className={classes.badge}>
+                {" "}
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
