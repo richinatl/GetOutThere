effdee316d8e2a29814454e88c5461df4b8cc02a
Richard Murphy <richinatl@gmail.com>
2022-12-29 13:08:59 -0500

    added footer component, link icons, styling changes

--- get-out-there/src/components/layout/Layout.js
+++ get-out-there/src/components/layout/Layout.js
@@ -1,6 +1,7 @@
 import MainNavigation from "./MainNavigation.js";
 import classes from "./Layout.module.css";
 import Banner from "./Banner";
+import Footer from "./Footer";
 
 function Layout(props) {
   return (
@@ -8,6 +9,7 @@ function Layout(props) {
       <MainNavigation />
       <Banner />
       <main className={classes.main}>{props.children}</main>
+      <Footer />
     </div>
   );
 }
